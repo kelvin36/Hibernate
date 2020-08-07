30bf3b4fd3a64d2f01554cf62735e5806a12731d
crist <crist@LAPTOP-5QRD4L1D>
2020-08-07 13:09:02 -0500

    version ultima clase

--- CursosOnlineHibernate/src/main/java/com/cursos_online/entidades/Estudiante.java
+++ CursosOnlineHibernate/src/main/java/com/cursos_online/entidades/Estudiante.java
@@ -1,8 +1,13 @@
 package com.cursos_online.entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Estudiante {
@@ -13,6 +18,13 @@ public class Estudiante {
 	private String nombre;
 	private String apellido;
 	
+	@OneToOne(mappedBy = "estudiante")
+	
+	private Telefono telefono;
+	
+	@OneToMany (mappedBy = "estudiante")
+	private List<Email>emails = new ArrayList<Email>();
+	
 	public Estudiante() {}
 	
 	public Estudiante(String nombre, String apellido) {
@@ -21,32 +33,61 @@ public class Estudiante {
 		this.apellido = apellido;
 	
 	}
-	
-	
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
-	@Override
-	public String toString() {
-		return "Estudiante [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+	public Telefono getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(Telefono telefono) {
+		this.telefono = telefono;
 	}
 	
 	
+
+	public List<Email> getEmails() {
+		return emails;
+	}
+
+	public void setEmails(List<Email> emails) {
+		this.emails = emails;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono
+				+ ", emails=" + emails + "]";
+	}
+
+	
+	
+	
+	
+	
+	
 }
+	
+	
